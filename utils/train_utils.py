149fa1a2dac3c148fd968ea1ffb703ff64d80165
Sean <xue0901@connect.hku.hk>
2021-06-24 11:24:13 +0800

    0624

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -50,11 +50,6 @@ class LR_Scheduler:
             param_group['lr'] = lr
 
     def warmup_schl(self,optimizer, iteration, epoch_size):
-<<<<<<< HEAD
-        print('iteration: ',iteration)
-        print('epoch:', epoch_size)
-=======
->>>>>>> adc85934ae4c4adc9c21e3cc3f6e73c456f0a022
         self.lr = 1e-6 + (config.hyp['lr0'] - 1e-6) * iteration / (epoch_size * config.warm_up)
         for param_group in optimizer.param_groups:
             param_group['lr'] = self.lr
